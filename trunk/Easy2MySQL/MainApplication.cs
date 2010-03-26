ab130168ec34ff95b5a07a78caae6ccc015e4cc5
정재성 <@5ef9371d-5e9b-823b-e305-aff914a05412>
2010-03-26 07:33:51 +0000

    MainForm 및 테마변경 기능 구현

---
+++ trunk/Easy2MySQL/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿
+// MainApplication.cs
+/// 
+
+namespace Easy2
+{
+	class MainApplication
+	{
+		// @breif 메인 함수입니다.
+		// @param args : 
+		public static void Main(string[] args)
+		{
+			System.Windows.Forms.Application.Run(new MainForm());
+		}
+	}
+}

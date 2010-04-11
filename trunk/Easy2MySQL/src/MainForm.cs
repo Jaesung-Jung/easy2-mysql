21641318d8c3b34401e908c80eacb7ab45c64e41
정재성 <@5ef9371d-5e9b-823b-e305-aff914a05412>
2010-04-11 09:27:03 +0000

    시작클래스 이름을 Program으로 변경 및 소스코드 구조 변경

--- trunk/Easy2MySQL/src/MainForm.cs
+++ trunk/Easy2MySQL/src/MainForm.cs
@@ -22,42 +22,6 @@ namespace Easy2
 				// Using StyleManager change the style and color tinting
 				StyleManager.ChangeStyle(style, Color.Empty);
 			}
-			else if(source.CommandParameter is Color)
-			{
-				StyleManager.ColorTint = (Color)source.CommandParameter;
-			}
 		}
-
-		#region Automatic Color Scheme creation based on the selected color table
-		private bool colorSelected = false;
-		private eStyle baseStyle = eStyle.Office2010Silver;
-		private void customColor_ExpandChange(object sender, EventArgs e)
-		{
-			if(this.customColor.Expanded)
-			{
-				// Remember the starting color scheme to apply if no color is selected during live-preview
-				colorSelected = false;
-				baseStyle = StyleManager.Style;
-			}
-			else
-			{
-				if(!colorSelected)
-				{
-					StyleManager.ChangeStyle(baseStyle, Color.Empty);
-				}
-			}
-		}
-
-		private void customColor_ColorPreview(object sender, ColorPreviewEventArgs e)
-		{
-			StyleManager.ColorTint = e.Color;
-		}
-
-		private void customColor_SelectedColorChanged(object sender, EventArgs e)
-		{
-			colorSelected = true; // Indicate that color was selected for buttonStyleCustom_ExpandChange method
-			customColor.CommandParameter = customColor.SelectedColor;
-		}
-		#endregion
 	}
 }

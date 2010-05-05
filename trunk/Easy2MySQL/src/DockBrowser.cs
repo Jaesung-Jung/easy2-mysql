1ef06b696b867c74dbc62cbce53753ac52940c31
정재성 <@5ef9371d-5e9b-823b-e305-aff914a05412>
2010-05-05 22:22:51 +0000

    DockManager, DockBrowser 클래스 추가 및 이전 ObjectBrowser 제거

---
+++ trunk/Easy2MySQL/src/DockBrowser.cs
@@ -0,0 +1,37 @@
+﻿
+// DockBrowser.cs
+//
+using DevComponents.DotNetBar;
+
+namespace Easy2
+{
+	/// <summary>
+	/// 도킹 브라우저 클래스입니다.
+	/// </summary>
+	class DockBrowser : Bar
+	{
+		/// <summary>
+		/// 생성자입니다.
+		/// </summary>
+		/// <param name="captionText">캡션 문자열입니다.</param>
+		public DockBrowser(string captionText) : base(captionText)
+		{
+			this.m_dockItem = new DockContainerItem();
+			//
+			// m_dockBar
+			//
+			this.CanHide = true;
+			this.Stretch = true;
+			this.LayoutType = eLayoutType.DockContainer;
+			this.Style = eDotNetBarStyle.StyleManagerControlled;
+			this.GrabHandleStyle = eGrabHandleStyle.CaptionDotted;
+			this.Items.Add(this.m_dockItem);
+			//
+			// m_dockItem
+			//
+			this.m_dockItem.Text = captionText;
+		}
+
+		protected DockContainerItem m_dockItem;
+	}
+}
---
+++ trunk/Easy2MySQL/src/ObjectBrowser.cs
@@ -0,0 +1,29 @@
+﻿
+// ObjectBrowser.cs
+//
+using System.Drawing;
+using DevComponents.DotNetBar;
+
+namespace Easy2
+{
+	/// <summary>
+	/// 오브젝트 브라우저 클래스입니다.
+	/// </summary>
+	class ObjectBrowser : DockBrowser
+	{
+		/// <summary>
+		/// 생성자입니다.
+		/// </summary>
+		public ObjectBrowser() : base("Object Browser")
+		{
+		}
+
+		/// <summary>
+		/// 생성자입니다.
+		/// </summary>
+		/// <param name="captionText">캡션 문자열입니다.</param>
+		public ObjectBrowser(string captionText) : base(captionText)
+		{
+		}
+	}
+}

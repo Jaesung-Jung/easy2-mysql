a6fb8360892fdd368c511e399a72c17dc26416ef
정재성 <@5ef9371d-5e9b-823b-e305-aff914a05412>
2010-05-31 18:55:14 +0000

    ConnectForm 추가

---
+++ Easy2MySQL/src/BaseForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Easy2
+{
+	partial class BaseForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.m_ribbonControl = new DevComponents.DotNetBar.RibbonControl();
+			this.SuspendLayout();
+			// 
+			// m_ribbonControl
+			// 
+			// 
+			// 
+			// 
+			this.m_ribbonControl.BackgroundStyle.Class = "";
+			this.m_ribbonControl.CaptionVisible = true;
+			this.m_ribbonControl.Dock = System.Windows.Forms.DockStyle.Top;
+			this.m_ribbonControl.KeyTipsFont = new System.Drawing.Font("Tahoma", 7F);
+			this.m_ribbonControl.Location = new System.Drawing.Point(4, 1);
+			this.m_ribbonControl.Name = "m_ribbonControl";
+			this.m_ribbonControl.Padding = new System.Windows.Forms.Padding(0, 0, 0, 2);
+			this.m_ribbonControl.Size = new System.Drawing.Size(476, 27);
+			this.m_ribbonControl.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+			this.m_ribbonControl.TabGroupHeight = 14;
+			this.m_ribbonControl.TabIndex = 0;
+			this.m_ribbonControl.Text = "ribbonControl";
+			// 
+			// BaseForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(484, 359);
+			this.Controls.Add(this.m_ribbonControl);
+			this.Name = "BaseForm";
+			this.Text = "BaseForm";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private DevComponents.DotNetBar.RibbonControl m_ribbonControl;
+	}
+}
---
+++ Easy2MySQL/src/ConnectForm.cs
@@ -0,0 +1,66 @@
+﻿
+// ConnectForm.cs
+//
+using System.Drawing;
+using System.Windows.Forms;
+using DevComponents.DotNetBar;
+
+namespace Easy2
+{
+	/// <summary>
+	/// 연결폼 클래스입니다.
+	/// </summary>
+	public partial class ConnectForm : BaseForm
+	{
+		/// <summary>
+		/// 생성자입니다.
+		/// </summary>
+		public ConnectForm()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// 폼이 로드될때 발생합니다.
+		/// </summary>
+		/// <param name="e">이벤트 객체입니다.</param>
+		protected override void OnLoad(System.EventArgs e)
+		{
+			base.OnLoad(e);
+
+			this.m_tabControl.ColorScheme.TabBackground = this.BackColor;
+			this.m_tabControl.ColorScheme.TabBackground2 = this.BackColor;
+
+			InitializeTabPanelBackColor();
+			InitializeLabelBackColor();
+		}
+
+		/// <summary>
+		/// 탭 패널의 배경색을 초기화합니다.
+		/// </summary>
+		private void InitializeTabPanelBackColor()
+		{
+			this.m_mysqlTabPanel.Style.BackColor1.Color = this.BackColor;
+			this.m_mysqlTabPanel.Style.BackColor2.Color = this.BackColor;
+			this.m_httpTabPanel.Style.BackColor1.Color = this.BackColor;
+			this.m_httpTabPanel.Style.BackColor2.Color = this.BackColor;
+			this.m_sshTabPanel.Style.BackColor1.Color = this.BackColor;
+			this.m_sshTabPanel.Style.BackColor2.Color = this.BackColor;
+			this.m_sslTabPanel.Style.BackColor1.Color = this.BackColor;
+			this.m_sslTabPanel.Style.BackColor2.Color = this.BackColor;
+		}
+
+		/// <summary>
+		/// 레이블들의 배경색을 초기화합니다.
+		/// </summary>
+		private void InitializeLabelBackColor()
+		{
+			this.m_mysqlHostLabel.BackColor = this.BackColor;
+			this.m_mysqlUserLabel.BackColor = this.BackColor;
+			this.m_mysqlPasswordLabel.BackColor = this.BackColor;
+			this.m_mysqlPortLabel.BackColor = this.BackColor;
+			this.m_mysqlDatabaseLabel.BackColor = this.BackColor;
+			this.m_savePasswordCheck.BackColor = this.BackColor;
+		}
+	}
+}

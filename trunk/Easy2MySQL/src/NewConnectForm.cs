c78f262b3c1a6001ba6ea9a49b845b7ce0c1ff47
정재성 <@5ef9371d-5e9b-823b-e305-aff914a05412>
2010-06-08 11:53:59 +0000

    ConnectForm 저장관련 기능구현

---
+++ trunk/Easy2MySQL/src/NewConnectForm.cs
@@ -0,0 +1,37 @@
+﻿
+// NewConnectForm.cs
+//
+using System.Windows.Forms;
+
+namespace Easy2
+{
+	public partial class NewConnectForm : BaseForm
+	{
+		public NewConnectForm(ConnectForm handle)
+		{
+			this.m_connectFormHandle = handle;
+			InitializeComponent();
+		}
+
+		private void m_connectNameText_TextChanged(object sender, System.EventArgs e)
+		{
+			this.m_commitButton.Enabled = this.m_connectNameText.TextLength == 0 ? false : true;
+		}
+
+		private void m_cancelButton_Click(object sender, System.EventArgs e)
+		{
+			this.DialogResult = DialogResult.Cancel;
+			this.Dispose(true);
+		}
+
+		private void m_commitButton_Click(object sender, System.EventArgs e)
+		{
+			this.m_connectFormHandle.ConnectInfo.ConnectName = this.m_connectNameText.Text;
+			this.DialogResult = DialogResult.OK;
+			this.Close();
+//			this.Dispose(true);
+		}
+
+		ConnectForm m_connectFormHandle;
+	}
+}

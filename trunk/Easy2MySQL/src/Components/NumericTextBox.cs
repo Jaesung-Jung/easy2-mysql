f0e92249bdac6300237af76bffdef49af81a3591
정재성 <@5ef9371d-5e9b-823b-e305-aff914a05412>
2010-08-31 07:33:29 +0000

    NumericTextBox 컴포넌트 추가

---
+++ trunk/Easy2MySQL/src/Components/NumericTextBox.cs
@@ -0,0 +1,30 @@
+﻿
+// NumericTextBox.cs
+//
+using System;
+using System.Windows.Forms;
+using DevComponents.DotNetBar.Controls;
+
+namespace Easy2.Components
+{
+	/// <summary>
+	/// 숫자만 입력가능한 텍스트박스입니다.
+	/// 숫자가 아닌 입력이 들어올 때 풍선 팁을 보여줍니다.
+	/// </summary>
+	class NumericTextBox : TextBoxX
+	{
+		/// <summary>
+		/// 키가 눌러지면 호출됩니다.
+		/// </summary>
+		/// <param name="e">이벤트정보를 가진 객체입니다.</param>
+		protected override void OnKeyPress(KeyPressEventArgs e)
+		{
+			char key = e.KeyChar;
+			if(Char.IsLetter(key) || Char.IsWhiteSpace(key) || Char.IsSymbol(key) || Char.IsPunctuation(key))
+			{
+				e.Handled = true;
+			}
+			base.OnKeyPress(e);
+		}
+	}
+}

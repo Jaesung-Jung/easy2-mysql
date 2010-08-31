ae34f67f6ad39479488a9fa2f3846cf3f18be196
정재성 <@5ef9371d-5e9b-823b-e305-aff914a05412>
2010-08-31 03:37:27 +0000

    AdvPropertiesForm 디자인 완료

--- Easy2MySQL/src/Forms/AdvPropertiesForm.Designer.cs
+++ Easy2MySQL/src/Forms/AdvPropertiesForm.Designer.cs
@@ -39,14 +39,24 @@
 			this.m_commentLabel = new DevComponents.DotNetBar.LabelX();
 			this.m_commentText = new DevComponents.DotNetBar.Controls.TextBoxX();
 			this.m_groupBox = new System.Windows.Forms.GroupBox();
-			this.m_autoincrLabel = new DevComponents.DotNetBar.LabelX();
-			this.m_autoincrText = new DevComponents.DotNetBar.Controls.TextBoxX();
-			this.m_avgrowLabel = new DevComponents.DotNetBar.LabelX();
-			this.m_avgrowText = new DevComponents.DotNetBar.Controls.TextBoxX();
-			this.m_minimumLabel = new DevComponents.DotNetBar.LabelX();
-			this.m_minimumText = new DevComponents.DotNetBar.Controls.TextBoxX();
 			this.m_maximumLabel = new DevComponents.DotNetBar.LabelX();
+			this.m_minimumLabel = new DevComponents.DotNetBar.LabelX();
+			this.m_avgrowLabel = new DevComponents.DotNetBar.LabelX();
+			this.m_autoincrLabel = new DevComponents.DotNetBar.LabelX();
+			this.m_chunksLabel = new DevComponents.DotNetBar.LabelX();
+			this.m_autoincrText = new DevComponents.DotNetBar.Controls.TextBoxX();
+			this.m_avgrowText = new DevComponents.DotNetBar.Controls.TextBoxX();
+			this.m_minimumText = new DevComponents.DotNetBar.Controls.TextBoxX();
+			this.m_maximumText = new DevComponents.DotNetBar.Controls.TextBoxX();
 			this.textBoxX1 = new DevComponents.DotNetBar.Controls.TextBoxX();
+			this.m_chunksizeLabel = new DevComponents.DotNetBar.LabelX();
+			this.m_chunksizeText = new DevComponents.DotNetBar.Controls.TextBoxX();
+			this.m_delayLabel = new DevComponents.DotNetBar.LabelX();
+			this.m_delayCombo = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+			this.m_rowformatLabel = new DevComponents.DotNetBar.LabelX();
+			this.m_rowformatCombo = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+			this.m_commitButton = new DevComponents.DotNetBar.ButtonX();
+			this.m_closeButton = new DevComponents.DotNetBar.ButtonX();
 			this.m_groupBox.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -183,71 +193,41 @@
 			// 
 			// m_groupBox
 			// 
+			this.m_groupBox.Controls.Add(this.m_rowformatCombo);
+			this.m_groupBox.Controls.Add(this.m_rowformatLabel);
+			this.m_groupBox.Controls.Add(this.m_delayCombo);
+			this.m_groupBox.Controls.Add(this.m_delayLabel);
+			this.m_groupBox.Controls.Add(this.m_chunksizeText);
+			this.m_groupBox.Controls.Add(this.m_chunksizeLabel);
 			this.m_groupBox.Controls.Add(this.textBoxX1);
-			this.m_groupBox.Controls.Add(this.m_maximumLabel);
+			this.m_groupBox.Controls.Add(this.m_maximumText);
 			this.m_groupBox.Controls.Add(this.m_minimumText);
-			this.m_groupBox.Controls.Add(this.m_minimumLabel);
 			this.m_groupBox.Controls.Add(this.m_avgrowText);
-			this.m_groupBox.Controls.Add(this.m_avgrowLabel);
 			this.m_groupBox.Controls.Add(this.m_autoincrText);
+			this.m_groupBox.Controls.Add(this.m_chunksLabel);
+			this.m_groupBox.Controls.Add(this.m_maximumLabel);
+			this.m_groupBox.Controls.Add(this.m_minimumLabel);
+			this.m_groupBox.Controls.Add(this.m_avgrowLabel);
 			this.m_groupBox.Controls.Add(this.m_autoincrLabel);
 			this.m_groupBox.Location = new System.Drawing.Point(12, 98);
 			this.m_groupBox.Name = "m_groupBox";
-			this.m_groupBox.Size = new System.Drawing.Size(428, 233);
+			this.m_groupBox.Size = new System.Drawing.Size(428, 157);
 			this.m_groupBox.TabIndex = 10;
 			this.m_groupBox.TabStop = false;
 			// 
-			// m_autoincrLabel
+			// m_maximumLabel
 			// 
-			this.m_autoincrLabel.AutoSize = true;
-			// 
-			// 
-			// 
-			this.m_autoincrLabel.BackgroundStyle.Class = "";
-			this.m_autoincrLabel.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-			this.m_autoincrLabel.Location = new System.Drawing.Point(15, 24);
-			this.m_autoincrLabel.Name = "m_autoincrLabel";
-			this.m_autoincrLabel.Size = new System.Drawing.Size(102, 21);
-			this.m_autoincrLabel.TabIndex = 0;
-			this.m_autoincrLabel.Text = "&Auto Increment";
-			// 
-			// m_autoincrText
+			this.m_maximumLabel.AutoSize = true;
 			// 
 			// 
 			// 
-			// 
-			this.textBoxX1.Border.Class = "TextBoxBorder";
-			this.textBoxX1.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-			this.m_autoincrText.Location = new System.Drawing.Point(123, 22);
-			this.m_autoincrText.Name = "m_autoincrText";
-			this.m_autoincrText.Size = new System.Drawing.Size(78, 25);
-			this.m_autoincrText.TabIndex = 1;
-			// 
-			// m_avgrowLabel
-			// 
-			this.m_avgrowLabel.AutoSize = true;
-			// 
-			// 
-			// 
-			this.m_avgrowLabel.BackgroundStyle.Class = "";
-			this.m_avgrowLabel.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-			this.m_avgrowLabel.Location = new System.Drawing.Point(226, 24);
-			this.m_avgrowLabel.Name = "m_avgrowLabel";
-			this.m_avgrowLabel.Size = new System.Drawing.Size(107, 21);
-			this.m_avgrowLabel.TabIndex = 2;
-			this.m_avgrowLabel.Text = "Avg &Row Length";
-			// 
-			// m_avgrowText
-			// 
-			// 
-			// 
-			// 
-			this.textBoxX1.Border.Class = "TextBoxBorder";
-			this.textBoxX1.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-			this.m_avgrowText.Location = new System.Drawing.Point(339, 22);
-			this.m_avgrowText.Name = "m_avgrowText";
-			this.m_avgrowText.Size = new System.Drawing.Size(78, 25);
-			this.m_avgrowText.TabIndex = 3;
+			this.m_maximumLabel.BackgroundStyle.Class = "";
+			this.m_maximumLabel.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+			this.m_maximumLabel.Location = new System.Drawing.Point(223, 55);
+			this.m_maximumLabel.Name = "m_maximumLabel";
+			this.m_maximumLabel.Size = new System.Drawing.Size(103, 21);
+			this.m_maximumLabel.TabIndex = 6;
+			this.m_maximumLabel.Text = "Ma&ximum Rows";
 			// 
 			// m_minimumLabel
 			// 
@@ -263,31 +243,95 @@
 			this.m_minimumLabel.TabIndex = 4;
 			this.m_minimumLabel.Text = "Min&imum Rows";
 			// 
-			// m_minimumText
+			// m_avgrowLabel
+			// 
+			this.m_avgrowLabel.AutoSize = true;
+			// 
+			// 
+			// 
+			this.m_avgrowLabel.BackgroundStyle.Class = "";
+			this.m_avgrowLabel.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+			this.m_avgrowLabel.Location = new System.Drawing.Point(223, 24);
+			this.m_avgrowLabel.Name = "m_avgrowLabel";
+			this.m_avgrowLabel.Size = new System.Drawing.Size(107, 21);
+			this.m_avgrowLabel.TabIndex = 2;
+			this.m_avgrowLabel.Text = "Avg &Row Length";
+			// 
+			// m_autoincrLabel
+			// 
+			this.m_autoincrLabel.AutoSize = true;
+			// 
+			// 
+			// 
+			this.m_autoincrLabel.BackgroundStyle.Class = "";
+			this.m_autoincrLabel.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+			this.m_autoincrLabel.Location = new System.Drawing.Point(15, 24);
+			this.m_autoincrLabel.Name = "m_autoincrLabel";
+			this.m_autoincrLabel.Size = new System.Drawing.Size(102, 21);
+			this.m_autoincrLabel.TabIndex = 0;
+			this.m_autoincrLabel.Text = "&Auto Increment";
+			// 
+			// m_chunksLabel
+			// 
+			this.m_chunksLabel.AutoSize = true;
+			// 
+			// 
+			// 
+			this.m_chunksLabel.BackgroundStyle.Class = "";
+			this.m_chunksLabel.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+			this.m_chunksLabel.Location = new System.Drawing.Point(15, 86);
+			this.m_chunksLabel.Name = "m_chunksLabel";
+			this.m_chunksLabel.Size = new System.Drawing.Size(51, 21);
+			this.m_chunksLabel.TabIndex = 8;
+			this.m_chunksLabel.Text = "C&hunks";
+			// 
+			// m_autoincrText
+			// 
+			// 
+			// 
+			// 
+			this.m_autoincrText.Border.Class = "TextBoxBorder";
+			this.m_autoincrText.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+			this.m_autoincrText.Location = new System.Drawing.Point(123, 22);
+			this.m_autoincrText.Name = "m_autoincrText";
+			this.m_autoincrText.Size = new System.Drawing.Size(79, 25);
+			this.m_autoincrText.TabIndex = 1;
+			// 
+			// m_avgrowText
 			// 
 			// 
 			// 
 			// 
 			this.textBoxX1.Border.Class = "TextBoxBorder";
 			this.textBoxX1.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-			this.m_minimumText.Location = new System.Drawing.Point(122, 53);
+			this.m_avgrowText.Location = new System.Drawing.Point(336, 22);
+			this.m_avgrowText.Name = "m_avgrowText";
+			this.m_avgrowText.Size = new System.Drawing.Size(79, 25);
+			this.m_avgrowText.TabIndex = 3;
+			// 
+			// m_minimumText
+			// 
+			// 
+			// 
+			// 
+			this.m_minimumText.Border.Class = "TextBoxBorder";
+			this.m_minimumText.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+			this.m_minimumText.Location = new System.Drawing.Point(123, 53);
 			this.m_minimumText.Name = "m_minimumText";
 			this.m_minimumText.Size = new System.Drawing.Size(79, 25);
 			this.m_minimumText.TabIndex = 5;
 			// 
-			// m_maximumLabel
-			// 
-			this.m_maximumLabel.AutoSize = true;
+			// m_maximumText
 			// 
 			// 
 			// 
-			this.m_maximumLabel.BackgroundStyle.Class = "";
-			this.m_maximumLabel.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-			this.m_maximumLabel.Location = new System.Drawing.Point(226, 55);
-			this.m_maximumLabel.Name = "m_maximumLabel";
-			this.m_maximumLabel.Size = new System.Drawing.Size(103, 21);
-			this.m_maximumLabel.TabIndex = 6;
-			this.m_maximumLabel.Text = "Ma&ximum Rows";
+			// 
+			this.textBoxX1.Border.Class = "TextBoxBorder";
+			this.textBoxX1.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+			this.m_maximumText.Location = new System.Drawing.Point(336, 53);
+			this.m_maximumText.Name = "m_maximumText";
+			this.m_maximumText.Size = new System.Drawing.Size(79, 25);
+			this.m_maximumText.TabIndex = 7;
 			// 
 			// textBoxX1
 			// 
@@ -296,16 +340,119 @@
 			// 
 			this.textBoxX1.Border.Class = "TextBoxBorder";
 			this.textBoxX1.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-			this.textBoxX1.Location = new System.Drawing.Point(339, 53);
+			this.textBoxX1.Location = new System.Drawing.Point(123, 84);
 			this.textBoxX1.Name = "textBoxX1";
 			this.textBoxX1.Size = new System.Drawing.Size(79, 25);
-			this.textBoxX1.TabIndex = 7;
+			this.textBoxX1.TabIndex = 9;
+			// 
+			// m_chunksizeLabel
+			// 
+			// 
+			// 
+			// 
+			this.m_chunksizeLabel.BackgroundStyle.Class = "";
+			this.m_chunksizeLabel.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+			this.m_chunksizeLabel.Location = new System.Drawing.Point(223, 85);
+			this.m_chunksizeLabel.Name = "m_chunksizeLabel";
+			this.m_chunksizeLabel.Size = new System.Drawing.Size(75, 23);
+			this.m_chunksizeLabel.TabIndex = 10;
+			this.m_chunksizeLabel.Text = "Chunk &Size";
+			// 
+			// m_chunksizeText
+			// 
+			// 
+			// 
+			// 
+			this.m_chunksizeText.Border.Class = "TextBoxBorder";
+			this.m_chunksizeText.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+			this.m_chunksizeText.Location = new System.Drawing.Point(336, 84);
+			this.m_chunksizeText.Name = "m_chunksizeText";
+			this.m_chunksizeText.Size = new System.Drawing.Size(79, 25);
+			this.m_chunksizeText.TabIndex = 11;
+			// 
+			// m_delayLabel
+			// 
+			this.m_delayLabel.AutoSize = true;
+			// 
+			// 
+			// 
+			this.m_delayLabel.BackgroundStyle.Class = "";
+			this.m_delayLabel.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+			this.m_delayLabel.Location = new System.Drawing.Point(15, 116);
+			this.m_delayLabel.Name = "m_delayLabel";
+			this.m_delayLabel.Size = new System.Drawing.Size(104, 21);
+			this.m_delayLabel.TabIndex = 12;
+			this.m_delayLabel.Text = "&Delay Key Write";
+			// 
+			// m_delayCombo
+			// 
+			this.m_delayCombo.DisplayMember = "Text";
+			this.m_delayCombo.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+			this.m_delayCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.m_delayCombo.FormattingEnabled = true;
+			this.m_delayCombo.ItemHeight = 19;
+			this.m_delayCombo.Location = new System.Drawing.Point(123, 115);
+			this.m_delayCombo.Name = "m_delayCombo";
+			this.m_delayCombo.Size = new System.Drawing.Size(79, 25);
+			this.m_delayCombo.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+			this.m_delayCombo.TabIndex = 13;
+			// 
+			// m_rowformatLabel
+			// 
+			this.m_rowformatLabel.AutoSize = true;
+			// 
+			// 
+			// 
+			this.m_rowformatLabel.BackgroundStyle.Class = "";
+			this.m_rowformatLabel.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+			this.m_rowformatLabel.Location = new System.Drawing.Point(223, 116);
+			this.m_rowformatLabel.Name = "m_rowformatLabel";
+			this.m_rowformatLabel.Size = new System.Drawing.Size(80, 21);
+			this.m_rowformatLabel.TabIndex = 14;
+			this.m_rowformatLabel.Text = "Row &Format";
+			// 
+			// m_rowformatCombo
+			// 
+			this.m_rowformatCombo.DisplayMember = "Text";
+			this.m_rowformatCombo.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+			this.m_rowformatCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.m_rowformatCombo.FormattingEnabled = true;
+			this.m_rowformatCombo.ItemHeight = 19;
+			this.m_rowformatCombo.Location = new System.Drawing.Point(336, 116);
+			this.m_rowformatCombo.Name = "m_rowformatCombo";
+			this.m_rowformatCombo.Size = new System.Drawing.Size(79, 25);
+			this.m_rowformatCombo.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+			this.m_rowformatCombo.TabIndex = 15;
+			// 
+			// m_commitButton
+			// 
+			this.m_commitButton.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+			this.m_commitButton.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+			this.m_commitButton.Location = new System.Drawing.Point(202, 269);
+			this.m_commitButton.Name = "m_commitButton";
+			this.m_commitButton.Size = new System.Drawing.Size(116, 25);
+			this.m_commitButton.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+			this.m_commitButton.TabIndex = 11;
+			this.m_commitButton.Text = "확인(&C)";
+			// 
+			// m_closeButton
+			// 
+			this.m_closeButton.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+			this.m_closeButton.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+			this.m_closeButton.Location = new System.Drawing.Point(324, 269);
+			this.m_closeButton.Name = "m_closeButton";
+			this.m_closeButton.Size = new System.Drawing.Size(116, 25);
+			this.m_closeButton.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+			this.m_closeButton.TabIndex = 12;
+			this.m_closeButton.Text = "닫기(&L)";
 			// 
 			// AdvPropertiesForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(452, 455);
+			this.ClientSize = new System.Drawing.Size(452, 306);
+			this.Controls.Add(this.m_closeButton);
+			this.Controls.Add(this.m_commitButton);
 			this.Controls.Add(this.m_groupBox);
 			this.Controls.Add(this.m_commentText);
 			this.Controls.Add(this.m_commentLabel);
@@ -317,11 +464,11 @@
 			this.Controls.Add(this.m_checksumLabel);
 			this.Controls.Add(this.m_engineCombo);
 			this.Controls.Add(this.m_engineLabel);
-			this.DoubleBuffered = true;
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "AdvPropertiesForm";
+			this.ShowIcon = false;
 			this.Text = "고급 설정";
 			this.m_groupBox.ResumeLayout(false);
 			this.m_groupBox.PerformLayout();
@@ -343,13 +490,23 @@
 		private DevComponents.DotNetBar.LabelX m_commentLabel;
 		private DevComponents.DotNetBar.Controls.TextBoxX m_commentText;
 		private System.Windows.Forms.GroupBox m_groupBox;
-		private DevComponents.DotNetBar.Controls.TextBoxX textBoxX1;
 		private DevComponents.DotNetBar.LabelX m_maximumLabel;
-		private DevComponents.DotNetBar.Controls.TextBoxX m_minimumText;
 		private DevComponents.DotNetBar.LabelX m_minimumLabel;
-		private DevComponents.DotNetBar.Controls.TextBoxX m_avgrowText;
 		private DevComponents.DotNetBar.LabelX m_avgrowLabel;
-		private DevComponents.DotNetBar.Controls.TextBoxX m_autoincrText;
 		private DevComponents.DotNetBar.LabelX m_autoincrLabel;
+		private DevComponents.DotNetBar.Controls.ComboBoxEx m_rowformatCombo;
+		private DevComponents.DotNetBar.LabelX m_rowformatLabel;
+		private DevComponents.DotNetBar.Controls.ComboBoxEx m_delayCombo;
+		private DevComponents.DotNetBar.LabelX m_delayLabel;
+		private DevComponents.DotNetBar.Controls.TextBoxX m_chunksizeText;
+		private DevComponents.DotNetBar.LabelX m_chunksizeLabel;
+		private DevComponents.DotNetBar.Controls.TextBoxX textBoxX1;
+		private DevComponents.DotNetBar.Controls.TextBoxX m_maximumText;
+		private DevComponents.DotNetBar.Controls.TextBoxX m_minimumText;
+		private DevComponents.DotNetBar.Controls.TextBoxX m_avgrowText;
+		private DevComponents.DotNetBar.Controls.TextBoxX m_autoincrText;
+		private DevComponents.DotNetBar.LabelX m_chunksLabel;
+		private DevComponents.DotNetBar.ButtonX m_commitButton;
+		private DevComponents.DotNetBar.ButtonX m_closeButton;
 	}
 }

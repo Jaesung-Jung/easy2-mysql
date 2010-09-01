81f68b329899001481bf891329bcb2f4035262bb
정재성 <@5ef9371d-5e9b-823b-e305-aff914a05412>
2010-09-01 07:05:25 +0000

    AdvPropertiesForm이 닫혀질 때 객체가 제거되지 않도록 함

--- Easy2MySQL/src/Forms/AdvPropertiesForm.cs
+++ Easy2MySQL/src/Forms/AdvPropertiesForm.cs
@@ -1,6 +1,8 @@
 ﻿
 // AdvPropertiesForm.cs
 //
+using System;
+using System.ComponentModel;
 
 namespace Easy2.Forms
 {
@@ -9,6 +11,33 @@ namespace Easy2.Forms
 		public AdvPropertiesForm()
 		{
 			InitializeComponent();
+		}
+
+		/// <summary>
+		/// 폼이 닫혀질 때 호출됩니다.
+		/// </summary>
+		/// <param name="e">이벤트정보를 가진 객체입니다.</param>
+		protected override void OnClosing(CancelEventArgs e)
+		{
+			e.Cancel = true;
+
+			if(this.Visible == true)
+			{
+				this.Visible = false;
+			}
+		}
+
+		/// <summary>
+		/// 닫기버튼을 클릭하면 호출됩니다.
+		/// </summary>
+		/// <param name="sender">이벤트를 발생시킨 객체입니다.</param>
+		/// <param name="e">이벤트정보를 가진 객체입니다.</param>
+		private void OnCloseButtonClick(object sender, EventArgs e)
+		{
+			if(this.Visible == true)
+			{
+				this.Visible = false;
+			}
 		}
 	}
 }

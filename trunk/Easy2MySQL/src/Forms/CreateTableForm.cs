d001210b729d4d6620199768ede83851e64c525b
정재성 <@5ef9371d-5e9b-823b-e305-aff914a05412>
2010-09-07 10:33:35 +0000

    예외처리 및 구조 수정

--- trunk/Easy2MySQL/src/Forms/CreateTableForm.cs
+++ trunk/Easy2MySQL/src/Forms/CreateTableForm.cs
@@ -14,7 +14,30 @@ namespace Easy2.Forms
 		public CreateTableForm()
 		{
 			InitializeComponent();
+			this.m_tableEditor.SelectionChanged += new EventHandler(this.OnTableEditorSelectionChanged);
 			base.TableEditorContainer = this.m_tableEditor;
+		}
+
+		/// <summary>
+		/// 선택된 셀이 변경되면 호출됩니다.
+		/// </summary>
+		/// <param name="sender">이벤트를 발생시킨 객체입니다.</param>
+		/// <param name="e">이벤트정보를 가진 객체입니다.</param>
+		void OnTableEditorSelectionChanged(object sender, EventArgs e)
+		{
+			if(this.m_tableEditor.CurrentCell.OwningRow.IsNewRow == false)
+			{
+				this.m_deleteRowButton.Enabled = true;
+			}
+			else
+			{
+				this.m_deleteRowButton.Enabled = false;
+			}
+		}
+
+		protected override void OnCommitButtonClick(object sender, EventArgs e)
+		{
+			base.OnCommitButtonClick(sender, e);
 		}
 
 		/// <summary>
@@ -28,6 +51,11 @@ namespace Easy2.Forms
 			base.OnAdvanceButtonClick(sender, e);
  		}
 
+		/// <summary>
+		/// 열 제거버튼을 클릭하면 호출됩니다.
+		/// </summary>
+		/// <param name="sender">이벤트를 발생시킨 객체입니다.</param>
+		/// <param name="e">이벤트정보를 가진 객체입니다.</param>
 		protected override void OnDeleteRowButtonClick(object sender, EventArgs e)
 		{
 			this.m_tableEditor.RemoveRow();

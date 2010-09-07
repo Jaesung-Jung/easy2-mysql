b7ac44551dd2911a52c6900b9433281124661ccf
정재성 <@5ef9371d-5e9b-823b-e305-aff914a05412>
2010-09-07 08:39:00 +0000

    TableOption 관련기능 추가

---
+++ Easy2MySQL/src/Classes/TableOption.cs
@@ -0,0 +1,113 @@
+﻿
+// TableOption.cs
+//
+
+namespace Easy2.Classes
+{
+	/// <summary>
+	/// 테이블의 여러 옵션들을 나타냅니다.
+	/// </summary>
+	public class TableOption
+	{
+		/// <summary>
+		/// 엔진을 나타냅니다.
+		/// </summary>
+		public string Engine
+		{
+			get { return this.m_engine; }
+			set { this.m_engine = value; }
+		}
+
+		/// <summary>
+		/// 문자셋을 나타냅니다.
+		/// </summary>
+		public string Charset
+		{
+			get { return this.m_charset; }
+			set { this.m_charset = value; }
+		}
+
+		/// <summary>
+		/// 콜레이션을 나타냅니다.
+		/// </summary>
+		public string Collation
+		{
+			get { return this.m_collation; }
+			set { this.m_collation = value; }
+		}
+
+		/// <summary>
+		/// 코멘트를 나타냅니다.
+		/// </summary>
+		public string Comment
+		{
+			get { return this.m_comment; }
+			set { this.m_comment = value; }
+		}
+
+		/// <summary>
+		/// 서식을 나타냅니다.
+		/// </summary>
+		public string Format
+		{
+			get { return this.m_format; }
+			set { this.m_format = value; }
+		}
+
+		/// <summary>
+		/// 체크섬을 나타냅니다.
+		/// </summary>
+		public string Checksum
+		{
+			get { return this.m_checksum; }
+			set { this.m_checksum = value; }
+		}
+
+		/// <summary>
+		/// 자동증가값을 나타냅니다.
+		/// </summary>
+		public string AutoIncrement
+		{
+			get { return this.m_autoIncrement; }
+			set { this.m_autoIncrement = value; }
+		}
+
+		/// <summary>
+		/// 평균 열의 길이를 나타냅니다.
+		/// </summary>
+		public string AvgRowLength
+		{
+			get { return this.m_avgRowLength; }
+			set { this.m_avgRowLength = value; }
+		}
+
+		/// <summary>
+		/// 최소 열의 길이를 나타냅니다.
+		/// </summary>
+		public string MinimumRows
+		{
+			get { return this.m_minimumRows; }
+			set { this.m_minimumRows = value; }
+		}
+
+		/// <summary>
+		/// 최대 열의 길이를 나타냅니다.
+		/// </summary>
+		public string MaximumRows
+		{
+			get { return this.m_maximumRows; }
+			set { this.m_maximumRows = value; }
+		}
+
+		private string m_engine;
+		private string m_charset;
+		private string m_collation;
+		private string m_comment;
+		private string m_format;
+		private string m_checksum;
+		private string m_autoIncrement;
+		private string m_avgRowLength;
+		private string m_minimumRows;
+		private string m_maximumRows;
+	}
+}
--- Easy2MySQL/src/Forms/AdvTablePropertiesForm.cs
+++ Easy2MySQL/src/Forms/AdvTablePropertiesForm.cs
@@ -36,9 +36,9 @@ namespace Easy2.Forms
 
 			this.m_rowformatCombo.Items.AddRange(new string[]{
 				"DEFAULT",
-				"Compressed",
-				"Dynamic",
-				"Fixed"
+				"COMPRESSED",
+				"DYNAMIC",
+				"FIXED"
 			});
 			this.m_rowformatCombo.SelectedIndex = 0;
 
@@ -76,5 +76,55 @@ namespace Easy2.Forms
 				this.Visible = false;
 			}
 		}
+
+		/// <summary>
+		/// 확인버튼을 클릭하면 호출됩니다.
+		/// </summary>
+		/// <param name="sender">이벤트를 발생시킨 객체입니다.</param>
+		/// <param name="e">이벤트정보를 가진 객체입니다.</param>
+		private void OnCommitButtonClick(object sender, EventArgs e)
+		{
+			UpdateData(true);
+			if(this.Visible == true)
+			{
+				this.Visible = false;
+			}
+		}
+
+		/// <summary>
+		/// 컨트롤에 값을 쓰거나 읽어옵니다.
+		/// </summary>
+		/// <param name="b">true이면 컨트롤에서 데이터를 읽어오고, false이면 컨트롤에 데이터를 씁니다.</param>
+		private void UpdateData(bool b)
+		{
+			if(b == true)
+			{
+				this.m_tableOption.Engine = this.m_engineCombo.SelectedItem.ToString();
+				this.m_tableOption.Charset = this.m_charsetCombo.SelectedItem.ToString();
+				this.m_tableOption.Collation = this.m_collationCombo.SelectedItem.ToString();
+				this.m_tableOption.Comment = this.m_commentText.Text;
+				this.m_tableOption.Format = this.m_rowformatCombo.SelectedItem.ToString();
+				this.m_tableOption.Checksum = this.m_checksumCombo.SelectedItem.ToString();
+				this.m_tableOption.AutoIncrement = this.m_autoincrText.Text;
+				this.m_tableOption.AvgRowLength = this.m_avgrowText.Text;
+				this.m_tableOption.MinimumRows = this.m_minimumText.Text;
+				this.m_tableOption.MaximumRows = this.m_maximumText.Text;
+			}
+			else
+			{
+
+			}
+		}
+
+		/// <summary>
+		/// 테이블옵션데이터를 나타냅니다.
+		/// </summary>
+		public TableOption OptionData
+		{
+			get { return this.m_tableOption; }
+			set { this.m_tableOption = value; }
+		}
+
+		private TableOption m_tableOption = new TableOption();
 	}
 }

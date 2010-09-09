1dc41bf89b21a636bdf1693ab81663594a609dfd
정재성 <@5ef9371d-5e9b-823b-e305-aff914a05412>
2010-09-09 07:39:47 +0000

    새로운 테이블명 입력창 추가

---
+++ trunk/Easy2MySQL/src/Forms/NewTableForm.cs
@@ -0,0 +1,33 @@
+﻿
+// NewTableForm.cs
+//
+using System;
+using DevComponents.DotNetBar;
+
+namespace Easy2.Forms
+{
+	public partial class NewTableForm : BaseForm
+	{
+		public NewTableForm()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnShown(EventArgs e)
+		{
+			base.OnShown(e);
+			this.m_tableNameText.Focus();
+			this.m_tableNameText.SelectAll();
+		}
+
+		private void OnTableNameTextChanged(object sender, EventArgs e)
+		{
+			this.m_commitButton.Enabled = this.m_tableNameText.TextLength > 0? true : false;
+		}
+
+		public string TableName
+		{
+			get { return this.m_tableNameText.Text; }
+		}
+	}
+}
